4b6a972ad2c7d0d41aec28510571439de9ec9afd
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-02-16 12:06:45 +0000

    updates to french flag

--- src/components/BasicComponents.jsx
+++ src/components/BasicComponents.jsx
@@ -18,8 +18,9 @@ const BasicComponents = () => {
         height: 100,
         padding: 20,
     }}>
-    <View style={{backgroundColor: 'green', flex: 0.2}} />
-    <View style={{backgroundColor: 'orange', flex: 0.5}} />
+    <View style={{backgroundColor: 'red', flex: 0.4}} />
+    <View style={{backgroundColor: 'white', flex: 0.4}} />
+    <View style={{backgroundColor: 'blue', flex: 0.4}} />
     <Text>Hello World!</Text>
     </View>
     <View>
